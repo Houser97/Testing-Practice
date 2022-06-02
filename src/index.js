9a8ff9236051dbf27577e0cc379f703ab1a64ef3
Houser97 <arturo.riverar97@gmail.com>
2022-06-02 12:35:49 -0500

    Finish calculator and ceasarCipher exercises

--- src/index.js
+++ src/index.js
@@ -18,4 +18,95 @@ function reverseString(a) {
   return result;
 }
 
-module.exports = { capitalize, reverseString };
+const calculator = (() => {
+  const add = (a, b) => a + b;
+  const sub = (a, b) => a - b;
+  const multiply = (a, b) => a * b;
+  const divide = (a, b) => {
+    if (b === 0) return "error";
+    return a / b;
+  };
+
+  return {
+    add, sub, multiply, divide,
+  };
+})();
+
+function getKeyByValue(object, value) {
+  return Object.keys(object).find((key) => object[key] === value);
+}
+
+function resetCountABC(numberLetter) {
+  if (numberLetter > 26) return numberLetter - 26;
+  if (numberLetter < 1) return numberLetter + 26;
+  return numberLetter;
+}
+
+function upperLetter(letter, shift, abc) {
+  let lowerLetter = letter.toLowerCase();
+  let numberLetter = "";
+  numberLetter = parseInt(abc[lowerLetter], 10);
+  numberLetter += shift;
+  numberLetter = resetCountABC(numberLetter);
+  numberLetter = String(numberLetter);
+  lowerLetter = String(getKeyByValue(abc, numberLetter));
+  return lowerLetter.toUpperCase();
+}
+
+function ceasarCipher(string, shift) {
+  const array = String(string);
+  const arrayLetters = array.split("");
+  const abc = {
+    a: "1",
+    b: "2",
+    c: "3",
+    d: "4",
+    e: "5",
+    f: "6",
+    g: "7",
+    h: "8",
+    i: "9",
+    j: "10",
+    k: "11",
+    l: "12",
+    m: "13",
+    n: "14",
+    o: "15",
+    p: "16",
+    q: "17",
+    r: "18",
+    s: "19",
+    t: "20",
+    u: "21",
+    v: "22",
+    w: "23",
+    x: "24",
+    y: "25",
+    z: "26",
+  };
+
+  let result = "";
+
+  for (let i = 0; i < arrayLetters.length; i += 1) {
+    const currentLetter = arrayLetters[i];
+    let numberLetter = "";
+    if (currentLetter === currentLetter.toUpperCase()
+    && currentLetter !== currentLetter.toLowerCase()) {
+      result += upperLetter(currentLetter, shift, abc);
+    } else if (currentLetter in abc) {
+      numberLetter = parseInt(abc[currentLetter], 10);
+      numberLetter += shift;
+      numberLetter = resetCountABC(numberLetter);
+      numberLetter = String(numberLetter);
+      result += String(getKeyByValue(abc, numberLetter));
+    } else {
+      result += currentLetter;
+    }
+  }
+
+  return result;
+}
+
+module.exports = {
+  capitalize, reverseString, calculator, ceasarCipher,
+};
--- src/index.test.js
+++ src/index.test.js
@@ -23,3 +23,59 @@ test("Reverse String", () => {
 test("Reverse String", () => {
   expect(index.reverseString(345)).toBe("543");
 });
+
+test("Calculator", () => {
+  expect(index.calculator.add(1, 5)).toBe(6);
+});
+
+test("Calculator", () => {
+  expect(index.calculator.sub(1, 5)).toBe(-4);
+});
+
+test("Calculator", () => {
+  expect(index.calculator.multiply(5, 5)).toBe(25);
+});
+
+test("Calculator", () => {
+  expect(index.calculator.multiply(5, -5)).toBe(-25);
+});
+
+test("Calculator", () => {
+  expect(index.calculator.divide(1, 0)).toBe("error");
+});
+
+test("Calculator", () => {
+  expect(index.calculator.divide(50, 5)).toBe(10);
+});
+
+test("ceasarCipher", () => {
+  expect(index.ceasarCipher("houser", 1)).toBe("ipvtfs");
+});
+
+test("ceasarCipher, check commas and spaces", () => {
+  expect(index.ceasarCipher("houser is, cool", 1)).toBe("ipvtfs jt, dppm");
+});
+
+test("ceasarCipher check capital letters", () => {
+  expect(index.ceasarCipher("houser Is, cool", 1)).toBe("ipvtfs Jt, dppm");
+});
+
+test("ceasarCipher check z letter to reset", () => {
+  expect(index.ceasarCipher("z", -2)).toBe("x");
+});
+
+test("ceasarCipher check z capital letter to reset", () => {
+  expect(index.ceasarCipher("Z", 5)).toBe("E");
+});
+
+test("ceasarCipher check a capital letter to reset", () => {
+  expect(index.ceasarCipher("A", -5)).toBe("V");
+});
+
+test("ceasarCipher check a  letter to reset", () => {
+  expect(index.ceasarCipher("a", -4)).toBe("w");
+});
+
+test("ceasarCipher check punctuation", () => {
+  expect(index.ceasarCipher("HouSer, is; cooL", 1)).toBe("IpvTfs, jt; dppM");
+});
